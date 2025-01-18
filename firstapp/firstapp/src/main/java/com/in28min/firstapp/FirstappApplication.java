4212b3812d4a6d67e81b8188f5da5d6ecb815759
Philemon Boaz <68799881+philemonboaz@users.noreply.github.com>
2025-01-18 07:19:00 +0530

    Add files via upload

---
+++ firstapp/firstapp/src/main/java/com/in28min/firstapp/FirstappApplication.java
@@ -0,0 +1,13 @@
+package com.in28min.firstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstappApplication.class, args);
+	}
+
+}
